45b8db257e4a789a08eb49c2b6cbc0471baa9362
jameswhatcott <james@whatcott.com>
2024-06-26 20:52:52 -0500

    beginning stuff

---
+++ Develop/index.js
@@ -0,0 +1,35 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    `#${fileName}
+    
+    ##Description
+    ${description}
+
+    ##Table of Contents
+
+    ##Installation
+
+    ##Usage
+
+    ##License
+
+    ##Contributing
+
+    ##Tests
+
+    ##Questions
+
+
+    `
+}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
