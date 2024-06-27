1f2338774694edfdc5b205679fed1eafc64d1fc4
jameswhatcott <james@whatcott.com>
2024-06-27 09:11:56 -0500

    some changes with questions and functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,42 +1,70 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "Title:",
+    "Description:",
+    "Installation Instructions:",
+    "Usage:",
+    "License:",
+    "Contribution Guidelines:",
+    "Test Instructions:"
+     ];
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: `${questions[0]}`,
+      name: "title",
+    },
+    {
+        type: "input",
+        message: `${questions[1]}`,
+        name: "description"
+    },
+    {
+        type: "input",
+        message: `${questions[2]}`,
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: `${questions[3]}`,
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: `${questions[4]}`,
+        name: "License"
+    },
+  ])
+  .then((response) => {
+    console.log(response);
+    fs.writeFile('README.md', generateREADME(response), (err) =>
+    err ? console.error(err) : console.log('Success!')
+    
+  )});
 
 // TODO: Create a function to write README file
-function writeToFile({fileName, }) {
-    `#${fileName}
-    
+
+function generateREADME (questions) {
+    `#`${questions[0]}`
     ##Description
-    ${description}
-
-    ##Table of Contents
-    ${tableOfContents}
-
-    ##Installation
-    ${installation}
-
-    ##Usage
-    ${usage}
-
-    ##License
-    ${license}
-
-    ##Contributing
-    ${contributing}
-
-    ##Tests
-    ${tests}
-
-    ##Questions
-    ${questions}
-
-
+    `{questions[1]}`
     `
 }
+
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
+
+
+
+
+  
